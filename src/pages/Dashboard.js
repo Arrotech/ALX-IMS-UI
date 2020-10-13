7ae08ac0ab30c7cf5f92cd20b2eef9c7a6bf27b1
Harun Gachanja <arrotechdesign@gmail.com>
2020-10-13 18:20:22 +0300

    Feature (Display Animations Collections)
    What does this pr do?
    
    Have a user able to view animations.
    
    Any screenshots?
    
    Any related pivotal stories?
    
    [Finishes #175250128]

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,16 +2,17 @@ import React from 'react'
 import '../assets/css/Dashboard.css'
 import Jokes from '../components/Jokes'
 import DashboardHeader from '../components/DashboardHeader'
+import AnimeCollection from '../components/AnimeCollection'
 
 function Dashboard() {
-
-
 
     return (
         <div className="dashboard">
             <DashboardHeader />
             <div className="dashboard__content">
                 <div className="dashboard__left">
+                    <h1>Animations</h1>
+                    <AnimeCollection/>
                 </div>
                 <div className="dashboard__right">
                     <Jokes />
